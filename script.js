cd4ebcba3e85c4180ef338006cd60d132e167859
EmersonTejada <emerson.8965@gmail.com>
2025-01-31 14:21:02 -0400

    Solution Foundation Rock, Paper, Scissors
    I completed the exercise proposed in the course using the knowledge acquired so far

---
+++ script.js
@@ -0,0 +1,91 @@
+// Diseñar algoritmo para que computerChoice sea un numero aleatorio y en base al numero entre 0 y 2 elegir si es Rock, Paper o Scissors
+let computerWinCount = 0;
+let humanWinCount = 0;
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "Rock";
+  } else if (choice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+// Diseñar algoritmo para la eleccion humana
+function getHumanChoice() {
+  let choice = prompt("Escriba Rock, Paper o Scissors");
+  choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+
+  if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
+    return choice;
+  } else {
+    return "Opcion Invalida";
+  }
+}
+
+// Diseñar algoritmo para jugar una sola Ronda
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === humanChoice) {
+    return console.log("Empate");
+  } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+    ++humanWinCount;
+    return console.log("Gana el humano");
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+    ++computerWinCount;
+    return console.log("Gana la maquina");
+  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    ++computerWinCount;
+    return console.log("Gana la maquina");
+  } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+    ++humanWinCount;
+    return console.log("Gana el humano");
+  } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+    ++humanWinCount;
+    return console.log("Gana el humano");
+  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+    ++computerWinCount;
+    return console.log("Gana la maquina");
+  } else {
+    return console.log("Opcion Invalida");
+  }
+}
+function playGame() { //Se juegan 5 rondas
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice();
+  console.log(computerChoice);
+  console.log(humanChoice);
+  playRound(computerChoice, humanChoice);
+  console.log(`Maquina: ${computerWinCount} - Humano: ${humanWinCount}`);
+
+  computerChoice = getComputerChoice();
+  humanChoice = getHumanChoice();
+  console.log(computerChoice);
+  console.log(humanChoice);
+  playRound(computerChoice, humanChoice);
+  console.log(`Maquina: ${computerWinCount} - Humano: ${humanWinCount}`);
+
+  computerChoice = getComputerChoice();
+  humanChoice = getHumanChoice();
+  console.log(computerChoice);
+  console.log(humanChoice);
+  playRound(computerChoice, humanChoice);
+  console.log(`Maquina: ${computerWinCount} - Humano: ${humanWinCount}`);
+
+  computerChoice = getComputerChoice();
+  humanChoice = getHumanChoice();
+  console.log(computerChoice);
+  console.log(humanChoice);
+  playRound(computerChoice, humanChoice);
+  console.log(`Maquina: ${computerWinCount} - Humano: ${humanWinCount}`);
+
+  computerChoice = getComputerChoice();
+  humanChoice = getHumanChoice();
+  console.log(computerChoice);
+  console.log(humanChoice);
+  playRound(computerChoice, humanChoice);
+  console.log(`Maquina: ${computerWinCount} - Humano: ${humanWinCount}`);
+}
+
+playGame();
